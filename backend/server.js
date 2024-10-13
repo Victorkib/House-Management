dd1a9f3605266b4ccf9b57c6df92aef978756094
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-10-13 16:33:52 +0300

    serverChange

--- backend/server.js
+++ backend/server.js
@@ -57,12 +57,12 @@ mongoose
 // Serve the static files from the React app (after building it)
 const __filename = fileURLToPath(import.meta.url); // Get the filename
 const __dirname = path.dirname(__filename); // Get the directory name
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../../../dist')));
 
 // API Routes (before handling frontend routes)
 app.use('/api', apiRoutes);
 
 // Handle any other routes and serve index.html (React's entry point)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../../dist', 'index.html'));
 });
