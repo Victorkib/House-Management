bc928a2286a4a833e2cd1847c389e58fda5e9aac
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-10-13 16:11:23 +0300

    chnageRender

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const devUrl = 'http://localhost:5500/api';
-const prodUrl = 'https://residential-0bacf0736179.herokuapp.com/api';
+// const prodUrl = 'https://residential-0bacf0736179.herokuapp.com/api';
+const prodUrl = 'https://house-management-backend.onrender.com/api';
 
 const apiRequest = axios.create({
   baseURL: prodUrl,
