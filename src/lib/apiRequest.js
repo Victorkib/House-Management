fd650b1d035700f46ceaa88c4a1d6b87b64e7a26
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-10-14 18:34:35 +0300

    changes

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const devUrl = 'http://localhost:5500/api';
-const prodUrl = 'https://residential-0bacf0736179.herokuapp.com/api';
+const prodUrl = 'https://residential-27ef77617daa.herokuapp.com/api';
 // const prodUrl = 'https://house-management-backend.onrender.com/api';
 
 const apiRequest = axios.create({
@@ -12,7 +12,6 @@ const apiRequest = axios.create({
 export default apiRequest;
 
 // import axios from 'axios';
-
 // const apiRequest = axios.create({
 //   withCredentials: true, // Keep credentials for cookies, etc.
 // });
