98ab22026e7f5f9964ba53f3c4e631af00d73430
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-10-14 15:53:13 +0300

    renderTrial2

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,20 +1,20 @@
-// import axios from 'axios';
-
-// // const devUrl = 'http://localhost:5500/api';
-// const prodUrl = 'https://residential-0bacf0736179.herokuapp.com/api';
-// // const prodUrl = 'https://house-management-backend.onrender.com/api';
-
-// const apiRequest = axios.create({
-//   baseURL: prodUrl,
-//   withCredentials: true,
-// });
-
-// export default apiRequest;
-
 import axios from 'axios';
 
+// const devUrl = 'http://localhost:5500/api';
+// const prodUrl = 'https://residential-0bacf0736179.herokuapp.com/api';
+const prodUrl = 'https://house-management-backend.onrender.com/api';
+
 const apiRequest = axios.create({
-  withCredentials: true, // Keep credentials for cookies, etc.
+  baseURL: prodUrl,
+  withCredentials: true,
 });
 
 export default apiRequest;
+
+// import axios from 'axios';
+
+// const apiRequest = axios.create({
+//   withCredentials: true, // Keep credentials for cookies, etc.
+// });
+
+// export default apiRequest;
