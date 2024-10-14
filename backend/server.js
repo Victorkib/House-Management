c502e38abb9c1204974839a79e7dbde0c419d147
Victorkib01~ <victor3720kibiwott@gmail.com>
2024-10-14 16:18:45 +0300

    serverChange

--- backend/server.js
+++ backend/server.js
@@ -26,12 +26,12 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-  res.setHeader('Pragma', 'no-cache');
-  res.setHeader('Expires', '0');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+//   res.setHeader('Pragma', 'no-cache');
+//   res.setHeader('Expires', '0');
+//   next();
+// });
 
 app.use(express.json()); // To handle JSON data
 app.use(express.urlencoded({ extended: true })); // To handle form data
